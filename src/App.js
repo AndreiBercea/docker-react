f5f3ba5781a558d996d430cb1c7249880677cfdb
Andrei Bercea <i.bercea@student.utwente.nl>
2021-08-26 11:00:20 +0000

    remove proxy env from dockerfile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"></img>
-        <p>Working</p>
+        <p>Working v2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
